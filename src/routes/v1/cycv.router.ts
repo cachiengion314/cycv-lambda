38f623a9bfce35414d78e4b2c2b343cc194dcb53
Hoang_Nam <cachiengion314@gmail.com>
2023-05-25 01:01:07 +0700

    update

--- src/routes/v1/cycv.router.ts
+++ src/routes/v1/cycv.router.ts
@@ -1,40 +1,47 @@
-const express = require(`express`);
-const router = express.Router();
-const controller = require(`../../controller/controller`);
-const authenticate = require(`../../services/authenticate`);
-const { isAuth } = require(`../../middleware/isAuth`);
+import { Request, Response, Router } from "express";
 
-router.post(`/auth/login`, authenticate.generateToken);
-router.post(`/auth/signup`, authenticate.createUser);
+// const controller = require(`../../controller/controller`);
+// const authenticate = require(`../../services/authenticate`);
+// const { isAuth } = require(`../../middleware/isAuth`);
+
+const router = Router();
+
+// router.post(`/auth/login`, authenticate.generateToken);
+// router.post(`/auth/signup`, authenticate.createUser);
 
 // public api
+// router.get(
+//   `/api/get-specific-savefile-showcase`,
+//   controller.getSpecificSaveFileShowcase
+// );
 router.get(
-  `/api/get-specific-savefile-showcase`,
-  controller.getSpecificSaveFileShowcase
+  `/api/get-all-savefile-showcase`,
+  async (req: Request, res: Response) => {
+    res.send("hello world from hoangnam");
+  }
 );
-router.get(`/api/get-all-savefile-showcase`, controller.getAllSaveFileShowCase);
-router.get(`/api/get-comments`, controller.getComments);
-// private api
-router.post(`/api/add-comment`, isAuth, controller.addComment);
-router.get(
-  `/api/get-all-savefile-showcase-of-user`,
-  isAuth,
-  controller.getAllSaveFileShowCaseOfUser
-);
-router.delete(
-  `/api/remove-savefile-showcase`,
-  isAuth,
-  controller.removeSaveFileShowCase
-);
-router.put(
-  `/api/update-savefile-showcase`,
-  isAuth,
-  controller.updateSaveFileShowCase
-);
-router.post(
-  `/api/add-savefile-showcase`,
-  isAuth,
-  controller.addSaveFileToShowCase
-);
+// router.get(`/api/get-comments`, controller.getComments);
+
+// router.post(`/api/add-comment`, isAuth, controller.addComment);
+// router.get(
+//   `/api/get-all-savefile-showcase-of-user`,
+//   isAuth,
+//   controller.getAllSaveFileShowCaseOfUser
+// );
+// router.delete(
+//   `/api/remove-savefile-showcase`,
+//   isAuth,
+//   controller.removeSaveFileShowCase
+// );
+// router.put(
+//   `/api/update-savefile-showcase`,
+//   isAuth,
+//   controller.updateSaveFileShowCase
+// );
+// router.post(
+//   `/api/add-savefile-showcase`,
+//   isAuth,
+//   controller.addSaveFileToShowCase
+// );
 
 export default router;
